3172741679cc8d43ef6d2a0d64fd38859928a942
Vivek  Dey <146002603+VivekDeyVD@users.noreply.github.com>
2024-01-16 20:49:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -81,7 +81,7 @@
     </div>
 
     <?php include 'partials/_footer.php';?>
-    <!-- Optional JavaScript -->
+    <!-- Optional JavaScript ->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
